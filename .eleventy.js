e8a3944b7bad3b6554a732a05338ab09f106d986
arpit-agr <arpit.agrawal2502@gmail.com>
2023-01-25 10:51:44 +0530

    update eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -36,27 +36,36 @@ module.exports = function (eleventyConfig) {
 			return code;
 		}
 	});
+	eleventyConfig.addFilter("postDateTime", (dateObj) => {
+		return DateTime.fromJSDate(dateObj)
+			.setZone("Asia/Calcutta")
+			.setLocale("en-IN")
+			.toFormat("ff ZZZZ");
 	});
-  eleventyConfig.addFilter("postDateTime", (dateObj) => {
-		return DateTime.fromJSDate(dateObj).setZone("Asia/Calcutta").setLocale('en-IN').toFormat('ff ZZZZ');
+	eleventyConfig.addFilter("addNbsp", (str) => {
+		if (!str) {
+			return;
+		}
+		let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
+		title = title.replace(/"(.*)"/g, '\\"$1\\"');
+		return title;
 	});
-  eleventyConfig.addFilter("addNbsp", (str) => {
-    if (!str) {
-      return;
-    }
-    let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
-    title = title.replace(/"(.*)"/g, '\\"$1\\"');
-    return title;
-  });
-  eleventyConfig.addFilter("isoDate", (dateObj) => {
+	eleventyConfig.addFilter("isoDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).setZone("Asia/Calcutta").toISODate();
 	});
-  eleventyConfig.addFilter("toISO", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).setZone("Asia/Calcutta").toISO();
-  });
-  eleventyConfig.addFilter("nextDay", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).setZone("Asia/Calcutta").plus({ days: 1 });
-  });
+	eleventyConfig.addFilter("toISO", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).setZone("Asia/Calcutta").toISO();
+	});
+	eleventyConfig.addFilter("nextDay", (dateObj) => {
+		return DateTime.fromJSDate(dateObj)
+			.setZone("Asia/Calcutta")
+			.plus({ days: 1 });
+	});
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj)
+			.setZone("Asia/Calcutta")
+			.toLocaleString(DateTime.DATE_MED);
+	});
 
 	//SHORTCODE
 	eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
@@ -68,105 +77,105 @@ module.exports = function (eleventyConfig) {
 	);
 	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
-  //Transforms
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if(process.env.NODE_ENV === "production" && this.outputPath && this.outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        collapseBooleanAttributes: true
-      });
-      return minified;
-    }
+	//Transforms
+	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+		// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+		if (
+			process.env.NODE_ENV === "production" &&
+			this.outputPath &&
+			this.outputPath.endsWith(".html")
+		) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+				collapseBooleanAttributes: true,
+			});
+			return minified;
+		}
 
-    return content;
-  });
+		return content;
+	});
 
-  // Collections
-	eleventyConfig.addCollection("pages", function(collectionApi) {
+	// Collections
+	eleventyConfig.addCollection("pages", function (collectionApi) {
 		return collectionApi.getFilteredByGlob("./src/pages/*.md");
 	});
-  eleventyConfig.addCollection("projects", function(collectionApi) {
-		return sortByDisplayOrder(collectionApi.getFilteredByGlob("./src/projects/*.md"));
+	eleventyConfig.addCollection("projects", function (collectionApi) {
+		return sortByDisplayOrder(
+			collectionApi.getFilteredByGlob("./src/projects/*.md")
+		);
 	});
-	eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./src/posts/*.md");
+	eleventyConfig.addCollection("posts", function (collectionApi) {
+		return collectionApi.getFilteredByGlob("./src/posts/*.md");
 	});
-  eleventyConfig.addCollection("links", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./src/links/*.md");
-  });
-  eleventyConfig.addCollection("stories", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./src/stories/*.md");
-  });
-  eleventyConfig.addCollection("bookshelf", function(collectionApi) {
-    if(process.env.NODE_ENV === "production") {
-      return collectionApi
-        .getFilteredByGlob("./src/bookshelf/*.md")
-        .sort(() => {
-          return 0.5 - Math.random();
-        });
-    }
-    else {
-      return collectionApi.getFilteredByGlob("./src/bookshelf/*.md");
-    }
-  });
+	eleventyConfig.addCollection("links", function (collectionApi) {
+		return collectionApi.getFilteredByGlob("./src/links/*.md");
+	});
+	eleventyConfig.addCollection("stories", function (collectionApi) {
+		return collectionApi.getFilteredByGlob("./src/stories/*.md");
+	});
+	eleventyConfig.addCollection("bookshelf", function (collectionApi) {
+		if (process.env.NODE_ENV === "production") {
+			return collectionApi
+				.getFilteredByGlob("./src/bookshelf/*.md")
+				.sort(() => {
+					return 0.5 - Math.random();
+				});
+		} else {
+			return collectionApi.getFilteredByGlob("./src/bookshelf/*.md");
+		}
+	});
 
-  //Markdown
-  let markdownIt = require("markdown-it");
+	//Markdown
+	let markdownIt = require("markdown-it");
 	let markdownItFootnote = require("markdown-it-footnote");
-  let options = {
-    html: true,
-    breaks: true,
-    linkify: true
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
 	};
-  let markdownLib = markdownIt(options).use(markdownItFootnote);
+	let markdownLib = markdownIt(options).use(markdownItFootnote);
 	eleventyConfig.setLibrary("md", markdownLib);
-  markdownLib.renderer.rules.footnote_caption = (tokens, idx/*, options, env, slf*/) => {
-    var n = Number(tokens[idx].meta.id + 1).toString();
-  
-    if (tokens[idx].meta.subId > 0) {
-      n += ':' + tokens[idx].meta.subId;
-    }
-  
-    return n;
-  }
-  markdownLib.renderer.rules.footnote_ref = (tokens, idx, options, env, slf) => {
+	markdownLib.renderer.rules.footnote_caption = (
+		tokens,
+		idx /*, options, env, slf*/
+	) => {
+		var n = Number(tokens[idx].meta.id + 1).toString();
+
+		if (tokens[idx].meta.subId > 0) {
+			n += ":" + tokens[idx].meta.subId;
+		}
+
+		return n;
+	};
+	markdownLib.renderer.rules.footnote_ref = (
+		tokens,
+		idx,
+		options,
+		env,
+		slf
+	) => {
 		var id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
 		var caption = slf.rules.footnote_caption(tokens, idx, options, env, slf);
 		var refid = id;
 		if (tokens[idx].meta.subId > 0) {
-			refid += ':' + tokens[idx].meta.subId;
+			refid += ":" + tokens[idx].meta.subId;
 		}
-	
+
 		return `
 			<sup class="footnote-ref">
 				<a href="#fn${id}" id="fnref${refid}"><span class="vh">Footnote</span>${caption}</a>
 			</sup>`;
-	}
-  markdownLib.renderer.rules.footnote_anchor = (tokens, idx, options, env, slf) => {
-		var id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
-		var caption = slf.rules.footnote_caption(tokens, idx, options, env, slf);
-		var refid = id;
-		
-		if (tokens[idx].meta.subId > 0) {
-			id += ':' + tokens[idx].meta.subId;
-		}
-		if (tokens[idx].meta.subId > 0) {
-			refid += ':' + tokens[idx].meta.subId;
-		}
+	};
 
-		return ``;
-	}
-
-  return {
-    // dataTemplateEngine: "njk",
+	return {
+		// dataTemplateEngine: "njk",
 		markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
 		dir: {
-			input: 'src',
-			output: 'public'
-		}
+			input: "src",
+			output: "public",
+		},
 	};
 };
